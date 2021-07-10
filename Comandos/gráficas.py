ecb6c370f52ff361e4c6035778435a121c195a73
Camilo Sabogal <lobogral@gmail.com>
2021-07-09 21:30:20 -0500

    Arreglo error en sección

--- Comandos/gráficas.py
+++ Comandos/gráficas.py
@@ -1,9 +1,9 @@
 from matplotlib import pyplot
 
-def dibujarGraficaPuntos(muestra, título, unidadMedida):
+def dibujarGraficaPuntos(muestra, sección, título, unidadMedida):
     muestra = sorted(muestra)
     fig, ax = pyplot.subplots(figsize=(9,1))
-    fig.canvas.manager.set_window_title('Análisis ' + título)
+    fig.canvas.manager.set_window_title(sección + ' Análisis ' + título)
     pyplot.subplots_adjust(bottom=0.6)
     ax.set_xlabel(unidadMedida)
     ax.hlines(0,muestra[0],muestra[len(muestra)-1], colors='k')
