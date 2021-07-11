918a2845d3ea2b9012baf7ab6e00c67bce55c4a6
Camilo Sabogal <lobogral@gmail.com>
2021-07-11 14:23:43 -0500

    Simplifico comandos

--- Comandos/otros.py
+++ Comandos/otros.py
@@ -1,7 +1,2 @@
-import sys
-
-def tamaño(muestra):
-    return len(muestra)
-
-def escribir(*objects, sep=' ', end='\n', file=sys.stdout, flush=False):
-    print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+tamaño = len
+escribir = print
