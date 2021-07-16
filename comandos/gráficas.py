522bc307779fc1088a2caf80bd324c33e7af9d18
Camilo Sabogal <lobogral@gmail.com>
2021-07-16 15:52:51 -0500

    Retiro subplots en gráfica

--- comandos/gráficas.py
+++ comandos/gráficas.py
@@ -1,4 +1,4 @@
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 import random
 
 def dibujar(tipo, diccionarios, títuloVentana, unidadMedida):
@@ -6,17 +6,16 @@ def dibujar(tipo, diccionarios, títuloVentana, unidadMedida):
         dibujarPuntos(diccionarios, títuloVentana, unidadMedida)
 
 def dibujarPuntos(diccionarios, títuloVentana, unidadMedida):
-    fig, ax = pyplot.subplots(figsize=(9,1))
-    fig.canvas.manager.set_window_title(títuloVentana)
+    plt.figure(títuloVentana, figsize=(9,1))
     for diccionario in diccionarios:
         color = diccionario['color']
         muestra = diccionario['muestra']
         label = diccionario['nombre'] if len(diccionarios)>1 else ""
-        ax.hlines(0,min(muestra),max(muestra), colors='k')
-        ax.plot(muestra, [0]*len(muestra), 'o', color=color, label=label)
+        plt.hlines(0,min(muestra),max(muestra), colors='k')
+        plt.plot(muestra, [0]*len(muestra), 'o', color=color, label=label)
     if len(diccionarios)>1:
-        pyplot.legend(bbox_to_anchor =(1.05, 1), loc='upper left')
-    ax.set_xlabel(unidadMedida)
-    ax.get_yaxis().set_visible(False)
-    pyplot.subplots_adjust(top=0.6, bottom=0.45, left=0.06, right=0.76)
-    pyplot.show()
+        plt.legend(bbox_to_anchor =(1.05, 1), loc='upper left')
+    plt.xlabel(unidadMedida)
+    plt.gca().axes.get_yaxis().set_visible(False)
+    plt.tight_layout()
+    plt.show()
