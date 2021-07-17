b13c62fdad7433ff7227386cd03e76c042f8ca7e
Camilo Sabogal <lobogral@gmail.com>
2021-07-17 16:19:16 -0500

    Establezco límite superior variable en gráficas

--- comandos/gráficas.py
+++ comandos/gráficas.py
@@ -6,6 +6,7 @@ def dibujar(tipo, diccionarios, títuloVentana, unidadMedida):
         dibujarPuntos(diccionarios, títuloVentana, unidadMedida)
 
 def dibujarPuntos(diccionarios, títuloVentana, unidadMedida):
+    limiteSuperior = establecerLimiteSuperior(diccionarios)
     diccionarios = agregarValoresy(diccionarios)
     plt.figure(títuloVentana, figsize=(9,1.5))
     for diccionario in diccionarios:
@@ -17,7 +18,7 @@ def dibujarPuntos(diccionarios, títuloVentana, unidadMedida):
         plt.plot(valoresx, valoresy, 'o', color=color, label=label)
     if len(diccionarios)>1: plt.legend(bbox_to_anchor =(1.05, 1), loc='upper left')
     plt.xlabel(unidadMedida)
-    plt.ylim(-1,4)
+    plt.ylim(-1,limiteSuperior)
     plt.gca().axes.get_yaxis().set_visible(False)
     plt.tight_layout()
     plt.show()
@@ -31,3 +32,7 @@ def agregarValoresy(diccionarios):
         diccionario['valoresx'] = diccionario.pop('muestra')
         diccionario['valoresy'] = listay
     return diccionarios
+
+def establecerLimiteSuperior(diccionarios):
+    valsx = [val for dic in diccionarios for val in dic['muestra']]
+    return max([valsx.count(val) for val in valsx])
