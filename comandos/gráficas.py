49f5820c6354609133ff0b773f56e5a99c04bb6c
Camilo Sabogal <lobogral@gmail.com>
2021-07-17 13:42:58 -0500

    Separo los puntos comunes en la gráfica

--- comandos/gráficas.py
+++ comandos/gráficas.py
@@ -6,15 +6,31 @@ def dibujar(tipo, diccionarios, títuloVentana, unidadMedida):
         dibujarPuntos(diccionarios, títuloVentana, unidadMedida)
 
 def dibujarPuntos(diccionarios, títuloVentana, unidadMedida):
-    plt.figure(títuloVentana, figsize=(9,1))
+    diccionarios = agregarValoresy(diccionarios)
+    plt.figure(títuloVentana, figsize=(9,1.5))
     for diccionario in diccionarios:
         color = diccionario['color']
-        muestra = diccionario['muestra']
+        valoresx = diccionario['valoresx']
+        valoresy = diccionario['valoresy']
         label = diccionario['nombre'] if len(diccionarios)>1 else ""
-        plt.hlines(0,min(muestra),max(muestra), colors='k')
-        plt.plot(muestra, [0]*len(muestra), 'o', color=color, label=label)
+        plt.hlines(0,min(valoresx),max(valoresx), colors='k')
+        plt.plot(valoresx, valoresy, 'o', color=color, label=label)
     if len(diccionarios)>1: plt.legend(bbox_to_anchor =(1.05, 1), loc='upper left')
     plt.xlabel(unidadMedida)
+    plt.ylim(-1,4)
     plt.gca().axes.get_yaxis().set_visible(False)
     plt.tight_layout()
     plt.show()
+
+def agregarValoresy(diccionarios):
+    valoresy = []
+    valoresx = []
+    inicio = 0
+    for diccionario in diccionarios:
+        diccionario['valoresx'] = diccionario.pop('muestra')
+        valoresx += diccionario['valoresx']
+        for i in range(inicio, len(valoresx)):
+            valoresy += [valoresx[0:i+1].count(valoresx[i])-1]
+        diccionario['valoresy'] = valoresy[inicio:len(valoresx)]
+        inicio += len(valoresx)
+    return diccionarios
