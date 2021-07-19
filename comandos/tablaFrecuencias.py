813683f39ab29b3d1bd24cf8e0e28b8c67cad5e7
Camilo Sabogal <lobogral@gmail.com>
2021-07-19 14:46:08 -0500

    Agrego otro ejemplo para corroborar

--- comandos/tablaFrecuencias.py
+++ comandos/tablaFrecuencias.py
@@ -7,10 +7,24 @@ datos = [2.0, 3.0, 0.3, 3.3, 1.3, 0.4,
          1.0, 6.0, 5.6, 6.0, 1.2, 0.2]
 
 paso = 0.1
-rango = 7.0
+mínimo = 0.0
+máximo = 7.0
 numDivisiones = 7
 
-div = rango/numDivisiones
+"""
+datos  = [2.2, 4.1, 3.5, 4.5, 3.2, 3.7, 3.0, 2.6,
+          3.4, 1.6, 3.1, 3.3, 3.8, 3.1, 4.7, 3.7,
+          2.5, 4.3, 3.4, 3.6, 2.9, 3.3, 3.9, 3.1,
+          3.3, 3.1, 3.7, 4.4, 3.2, 4.1, 1.9, 3.4,
+          4.7, 3.8, 3.2, 2.6, 3.9, 3.0, 4.2, 3.5]
+
+paso = 0.1
+mínimo = 1.5
+máximo = 5.0
+numDivisiones = 7
+"""
+
+div = (máximo-mínimo)/numDivisiones
 
 strIntCls = "Int. Cls.     "
 strPntMed = "Pnt. Med.    "
@@ -19,8 +33,8 @@ strFrecRel = "Frec. Rel."
 print(f'{strIntCls}{strPntMed}{strFrec}{strFrecRel}')
 for i in range(numDivisiones):
     # Hacen los cálculos
-    minCls = div*i
-    maxCls = div*(i+1) - paso
+    minCls = mínimo + div*i
+    maxCls = mínimo + div*(i+1) - paso
     pntMed = redondear((minCls+maxCls)/2,2)
     frec = len([dato for dato in datos if minCls<=dato<=maxCls])
     frecRel = redondear(frec/len(datos), 3)
