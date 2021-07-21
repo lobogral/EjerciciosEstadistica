d4de8efe6d4d9bd21e390a37049a2197b12746f6
Camilo Sabogal <lobogral@gmail.com>
2021-07-21 12:42:51 -0500

    Separo funcionalidades tabla frecuencias

--- comandos/tablaFrecuencias.py
+++ comandos/tablaFrecuencias.py
@@ -1,22 +1,41 @@
 from redondeo import *
 from tiposGráficas import histograma
 
-def imprimirTabla(datos, paso, mínimo, máximo, numDivisiones):
+def obtenerTabla(datos, paso, mínimo, máximo, numDivisiones):
+    tabla = {}
+    intsCls, pntsMed, frecs, frecsRel = [], [], [], []    
     div = (máximo-mínimo)/numDivisiones
+    for i in range(numDivisiones):
+        minCls = mínimo + div*i
+        maxCls = mínimo + div*(i+1) - paso
+        frec = len([dato for dato in datos if minCls<=dato<=maxCls])
+        intsCls += [f'{minCls}-{maxCls}']
+        pntsMed += [str((minCls+maxCls)/2)]
+        frecs += [frec]
+        frecsRel += [redondear(frec/len(datos), 3)]
+    tabla['intCls'] = intsCls
+    tabla['pntMed'] = pntsMed
+    tabla['frec'] = frecs
+    tabla['frecRel'] = frecsRel    
+    return tabla
+
+def imprimirTabla(tabla):
+    # Transpone datos
+    tablaTras = []
+    for vals in zip(*tabla.values()):
+        tablaTras += [dict(zip(tabla.keys(), vals))]
+    tabla = tablaTras
+    # Imprime datos
     strIntCls = "Int. Cls.     "
     strPntMed = "Pnt. Med.    "
     strFrec = "Frec.    "
     strFrecRel = "Frec. Rel."
     print(f'{strIntCls}{strPntMed}{strFrec}{strFrecRel}')
-    for i in range(numDivisiones):
-        # Hacen los cálculos
-        minCls = mínimo + div*i
-        maxCls = mínimo + div*(i+1) - paso
-        pntMed = (minCls+maxCls)/2
-        frec = len([dato for dato in datos if minCls<=dato<=maxCls])
-        frecRel = redondear(frec/len(datos), 3)
-        # Hacen las impresiones
-        intCls = f'{minCls}-{maxCls}'
+    for diccionario in tabla:
+        intCls = diccionario['intCls']
+        pntMed = diccionario['pntMed']
+        frec = diccionario['frec']
+        frecRel = diccionario['frecRel']
         print(intCls, end="")
         espacios = len(strIntCls) - len(intCls) + len(str(pntMed))
         print('{:>{}}'.format(pntMed, espacios), end="")
@@ -25,22 +44,12 @@ def imprimirTabla(datos, paso, mínimo, máximo, numDivisiones):
         espacios = len(strFrec) - len(str(frec)) + len(str(frecRel))
         print('{:>{}}'.format(frecRel, espacios))
 
-def establecerDatosHist(datos, paso, mínimo, máximo, numDivisiones):
-    datosHist = {}
-    valoresx = []
-    valoresy = []
-    div = (máximo-mínimo)/numDivisiones
-    for i in range(numDivisiones):
-        # Hacen los cálculos
-        minCls = mínimo + div*i
-        maxCls = mínimo + div*(i+1) - paso
-        valoresx += [str((minCls+maxCls)/2)]
-        frec = len([dato for dato in datos if minCls<=dato<=maxCls])
-        valoresy += [redondear(frec/len(datos), 3)]
-    # Asigna los valores
-    datosHist['valoresx'] = valoresx
-    datosHist['valoresy'] = valoresy
-    return datosHist
+def establecerDatosHist(tabla):
+    tabla['valoresx'] = tabla.pop('pntMed')
+    tabla['valoresy'] = tabla.pop('frecRel')
+    del tabla['frec']
+    del tabla['intCls']
+    return tabla
 
 datos = [2.0, 3.0, 0.3, 3.3, 1.3, 0.4,
          0.2, 6.0, 5.5, 6.5, 0.2, 2.3,
@@ -48,9 +57,9 @@ datos = [2.0, 3.0, 0.3, 3.3, 1.3, 0.4,
          4.5, 0.3, 1.5, 0.5, 2.5, 5.0,
          1.0, 6.0, 5.6, 6.0, 1.2, 0.2]
 
-imprimirTabla(datos, 0.1, 0.0, 7.0, 7)
-histograma.dibujar(establecerDatosHist(datos, 0.1, 0.0, 7.0, 7))
-
+tabla = obtenerTabla(datos, 0.1, 0.0, 7.0, 7)
+imprimirTabla(tabla)
+histograma.dibujar(establecerDatosHist(tabla))
 
 datos  = [2.2, 4.1, 3.5, 4.5, 3.2, 3.7, 3.0, 2.6,
           3.4, 1.6, 3.1, 3.3, 3.8, 3.1, 4.7, 3.7,
@@ -58,5 +67,6 @@ datos  = [2.2, 4.1, 3.5, 4.5, 3.2, 3.7, 3.0, 2.6,
           3.3, 3.1, 3.7, 4.4, 3.2, 4.1, 1.9, 3.4,
           4.7, 3.8, 3.2, 2.6, 3.9, 3.0, 4.2, 3.5]
 
-imprimirTabla(datos, 0.1, 1.5, 5.0, 7)
-histograma.dibujar(establecerDatosHist(datos, 0.1, 1.5, 5.0, 7))
+tabla = obtenerTabla(datos, 0.1, 1.5, 5.0, 7)
+imprimirTabla(tabla)
+histograma.dibujar(establecerDatosHist(tabla))
