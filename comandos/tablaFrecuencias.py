8da5ffb15633b00aace1a1db967de1ecc92aba88
Camilo Sabogal <lobogral@gmail.com>
2021-07-21 13:29:12 -0500

    Mejoro entendimiento de código

--- comandos/tablaFrecuencias.py
+++ comandos/tablaFrecuencias.py
@@ -2,30 +2,22 @@ from redondeo import *
 from tiposGráficas import histograma
 
 def obtenerTabla(datos, paso, mínimo, máximo, numDivisiones):
-    tabla = {}
-    intsCls, pntsMed, frecs, frecsRel = [], [], [], []    
+    tabla = []   
     div = (máximo-mínimo)/numDivisiones
     for i in range(numDivisiones):
         minCls = mínimo + div*i
         maxCls = mínimo + div*(i+1) - paso
         frec = len([dato for dato in datos if minCls<=dato<=maxCls])
-        intsCls += [f'{minCls}-{maxCls}']
-        pntsMed += [str((minCls+maxCls)/2)]
-        frecs += [frec]
-        frecsRel += [redondear(frec/len(datos), 3)]
-    tabla['intCls'] = intsCls
-    tabla['pntMed'] = pntsMed
-    tabla['frec'] = frecs
-    tabla['frecRel'] = frecsRel    
+        tabla += [{
+            'intCls': f'{minCls}-{maxCls}',
+            'pntMed': str((minCls+maxCls)/2),
+            'frec': frec,
+            'frecRel': redondear(frec/len(datos), 3)
+        }]   
     return tabla
 
-def imprimirTabla(tabla):
-    # Transpone datos
-    tablaTras = []
-    for vals in zip(*tabla.values()):
-        tablaTras += [dict(zip(tabla.keys(), vals))]
-    tabla = tablaTras
-    # Imprime datos
+def imprimirTabla(datos, paso, mínimo, máximo, numDivisiones):
+    tabla = obtenerTabla(datos, paso, mínimo, máximo, numDivisiones)
     strIntCls = "Int. Cls.     "
     strPntMed = "Pnt. Med.    "
     strFrec = "Frec.    "
@@ -44,12 +36,12 @@ def imprimirTabla(tabla):
         espacios = len(strFrec) - len(str(frec)) + len(str(frecRel))
         print('{:>{}}'.format(frecRel, espacios))
 
-def establecerDatosHist(tabla):
-    tabla['valoresx'] = tabla.pop('pntMed')
-    tabla['valoresy'] = tabla.pop('frecRel')
-    del tabla['frec']
-    del tabla['intCls']
-    return tabla
+def establecerDatosHist(datos, paso, mínimo, máximo, numDivisiones):
+    tabla = obtenerTabla(datos, paso, mínimo, máximo, numDivisiones)
+    hist = {}
+    hist['valoresx'] = [diccionario['pntMed'] for diccionario in tabla]
+    hist['valoresy'] = [diccionario['frecRel'] for diccionario in tabla]
+    return hist
 
 datos = [2.0, 3.0, 0.3, 3.3, 1.3, 0.4,
          0.2, 6.0, 5.5, 6.5, 0.2, 2.3,
@@ -57,9 +49,8 @@ datos = [2.0, 3.0, 0.3, 3.3, 1.3, 0.4,
          4.5, 0.3, 1.5, 0.5, 2.5, 5.0,
          1.0, 6.0, 5.6, 6.0, 1.2, 0.2]
 
-tabla = obtenerTabla(datos, 0.1, 0.0, 7.0, 7)
-imprimirTabla(tabla)
-histograma.dibujar(establecerDatosHist(tabla))
+imprimirTabla(datos, 0.1, 0.0, 7.0, 7)
+histograma.dibujar(establecerDatosHist(datos, 0.1, 0.0, 7.0, 7))
 
 datos  = [2.2, 4.1, 3.5, 4.5, 3.2, 3.7, 3.0, 2.6,
           3.4, 1.6, 3.1, 3.3, 3.8, 3.1, 4.7, 3.7,
@@ -67,6 +58,5 @@ datos  = [2.2, 4.1, 3.5, 4.5, 3.2, 3.7, 3.0, 2.6,
           3.3, 3.1, 3.7, 4.4, 3.2, 4.1, 1.9, 3.4,
           4.7, 3.8, 3.2, 2.6, 3.9, 3.0, 4.2, 3.5]
 
-tabla = obtenerTabla(datos, 0.1, 1.5, 5.0, 7)
-imprimirTabla(tabla)
-histograma.dibujar(establecerDatosHist(tabla))
+imprimirTabla(datos, 0.1, 1.5, 5.0, 7)
+histograma.dibujar(establecerDatosHist(datos, 0.1, 1.5, 5.0, 7))
