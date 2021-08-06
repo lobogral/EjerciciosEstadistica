269cfa4705d7582f2e0dd15d241d942e1b978f72
Camilo Sabogal <lobogral@gmail.com>
2021-08-06 13:48:51 -0500

    Agrego mas a la 7ma parte

--- src/capitulo-3/parte-3-1.py
+++ src/capitulo-3/parte-3-1.py
@@ -1,4 +1,4 @@
-desde sympy importar integrar
+desde sympy importar integrar, oo
 desde sympy.abc importar x, t
 desde sympy.functions importar exp
 desde fracciones importar Fracción
@@ -58,3 +58,58 @@ P = redondear(1 - F.subs(x,1000).evalf(), 4)
 escribir("b) P(X > 1000) =", P)
 P = redondear(F.subs(x,2000).evalf(), 4)
 escribir("c) P(X <= 2000) =", P)
+
+escribir("")
+escribir("3.29")
+f = 3*t**(-4)
+a = 1
+b = oo
+escribir("a) P(X > 1) =", integrar(f, (t,a,b)))
+F = integrar(f, (t,1,x))
+escribir("b) F(x) =")
+escribir("|", 0, "         , x < 1")
+escribir("|", F, ", x >= 1")
+P = redondear(1 - F.subs(x,4).evalf(), 4)
+escribir("c) P(X > 4) =", P)
+
+
+escribir("")
+escribir("3.31")
+f = Fracción(1,4)*exp(-t/4)
+a = 6
+b = oo
+P = redondear(integrar(f, (t,a,b)).evalf(),4)
+escribir("a) P(Y > 6) =", P)
+a = 0
+b = 1
+P = redondear(integrar(f, (t,a,b)).evalf(),4)
+escribir("b) P(0 < Y < 1) =", P)
+
+escribir("")
+escribir("3.33")
+f = (t**4)*(1-t)**3
+k = 1/integrar(f, (t,0,1))
+escribir("a) k =", k)
+a = 0
+b = 0.5
+P = redondear(integrar(k*f, (t,a,b)).evalf(),4)
+escribir("b) P(0 < Y < 0.5) =", P)
+a = 0.8
+b = 1
+P = redondear(integrar(k*f, (t,a,b)).evalf(),4)
+escribir("c) P(0.8 < Y < 1) =", P)
+
+
+escribir("")
+escribir("3.35")
+f = (t**4)*(1-t)**3
+k = 1/integrar(f, (t,0,1))
+escribir("a) k =", k)
+a = 0
+b = 0.5
+P = redondear(integrar(k*f, (t,a,b)).evalf(),4)
+escribir("b) P(0 < Y < 0.5) =", P)
+a = 0.8
+b = 1
+P = redondear(integrar(k*f, (t,a,b)).evalf(),4)
+escribir("c) P(0.8 < Y < 1) =", P)
