23088f2d3bd1efe575a7db47b9fb9e740260ef52
Camilo Sabogal <lobogral@gmail.com>
2021-08-09 11:14:00 -0500

    Agrego ejercicios distribución discreta en la septima parte

--- src/capitulo-3/parte-3-1.py
+++ src/capitulo-3/parte-3-1.py
@@ -1,10 +1,20 @@
-desde sympy importar Trozos
+desde sympy importar Trozos, C, Rational
 desde sympy.abc importar x, y
 desde sympy.functions importar exp
 desde fracciones importar Fracción
 desde redondeo importar redondear
 desde distribuciones importar continua como cont
+desde distribuciones importar discreta como disc
 
+escribir("3.5")
+f = x**2 + 4
+distribución = disc.Func2Dist(f, x, [0,1,2,3])
+escribir("a) c =", 1/disc.ProbTotal(distribución))
+f = C(2,x)*C(3,3-x)
+distribución = disc.Func2Dist(f, x, [0,1,2])
+escribir("b) c =", 1/disc.ProbTotal(distribución))
+
+escribir("")
 escribir("3.7")
 f = Trozos((x, (0<x) & (x<1)),
            (2-x, (1<=x) & (x<2)), 
@@ -23,6 +33,30 @@ escribir("a) Área bajo curva =", cont.ProbTotal(f,x))
 a = Fracción(1,4)
 b = Fracción(1,2)
 escribir("b) P(1/4 < X < 1/2) =", cont.Prob(f,x,a,b))
+
+escribir("")
+escribir("3.11")
+f = (C(2,x)*C(5,3-x))/C(7,3)
+print("Distribución:", disc.Func2Dist(f, x, [0,1,2]))
+
+escribir("")
+escribir("3.13")
+distribución = {0:0.41, 1:0.37, 2:0.16, 3:0.05, 4:0.01}
+escribir("F(x)=", disc.ProbAcum(distribución))
+
+escribir("")
+escribir("3.15")
+f = (C(2,x)*C(5,3-x))/C(7,3)
+distribución = disc.Func2Dist(f, x, [0,1,2])
+F = disc.ProbAcum(distribución)
+escribir("F(x)=", F)
+Fb = F.subs(x,1.9)
+Fa = F.subs(x,0.9)
+escribir("a) P(X = 1) =", Fb-Fa)
+Fb = F.subs(x,2)
+Fa = F.subs(x,0.1)
+escribir("b) P(0 < X <= 2) =", Fb-Fa)
+
 
 escribir("")
 escribir("3.17")
@@ -57,6 +91,19 @@ escribir("F(x) =", F)
 Fb = F.subs(x, 0.6)
 Fa = F.subs(x, 0.3)
 escribir("P(0.3 < X < 0.6) =", redondear(Fb-Fa, 4))
+
+escribir("")
+escribir("3.23")
+distribución = {-3:Fracción(1,27), 
+                -1:Fracción(6,27), 
+                 1:Fracción(12,27), 
+                 3:Fracción(8,27)}
+F = disc.ProbAcum(distribución)
+escribir("F(x)=", F)
+escribir("a) P(X > 0) =", 1-F.subs(x,0))
+Fb = F.subs(x,2.9)
+Fa = F.subs(x,-1)
+escribir("b) P(-1 <= X < 3) =", Fb-Fa)
 
 escribir("")
 escribir("3.27")
