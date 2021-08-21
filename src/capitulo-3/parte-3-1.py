bc9e4aaecfbe662cd6b9512a42066ed58726bcb9
Camilo Sabogal <lobogral@gmail.com>
2021-08-20 20:28:18 -0500

    Reescribo ejercicios utilizando herramientas ya trabajadas

--- src/capitulo-3/parte-3-1.py
+++ src/capitulo-3/parte-3-1.py
@@ -2,6 +2,7 @@ desde sympy importar Piecewise como Trozos
 desde sympy importar binomial como C
 desde sympy.abc importar x, y
 desde sympy.functions importar exp
+desde sympy.stats importar ContinuousRV
 desde fractions importar Fraction como Fracción
 desde redondeo importar redondear
 desde distribuciones importar continua como cont
@@ -17,23 +18,26 @@ escribir("b) c =", 1/disc.ProbTotal(distribución))
 
 escribir("")
 escribir("3.7")
-f = Trozos((x, (0<x) & (x<1)),
-           (2-x, (1<=x) & (x<2)), 
-           (0, otroCaso))
-b = 1.2
-escribir("a) P(X < 1.2) =", redondear(cont.ProbAcum(f,x,b),2))
-a = 0.5
-b = 1
-escribir("b) P(0.5 < X < 1) =", redondear(cont.Prob(f,x,a,b),3))
+fdp = Trozos((x, (0<x) & (x<1)),
+             (2-x, (1<=x) & (x<2)), 
+             (0, otroCaso))
+X = ContinuousRV(x, fdp)
+prob = redondear(cont.Prob(X <= 1.2), 2)
+escribir("a) P(X < 1.2) =", prob)
+prob = redondear(cont.Prob( (0.5 < X) & (X < 1) ), 3)
+escribir("b) P(0.5 < X < 1) =", prob)
 
 escribir("")
 escribir("3.9")
-f = Trozos((Fracción(2,5)*(x+2), (0<x) & (x<1)), 
-           (0, otroCaso))
-escribir("a) Área bajo curva =", cont.ProbTotal(f,x))
+fdp = Trozos((Fracción(2,5)*(x+2), (0<x) & (x<1)), 
+             (0, otroCaso))
+X = ContinuousRV(x, fdp)
+prob = cont.ProbTotal(fdp,x)
+escribir("a) Área bajo curva =", prob)
 a = Fracción(1,4)
 b = Fracción(1,2)
-escribir("b) P(1/4 < X < 1/2) =", cont.Prob(f,x,a,b))
+prob = cont.Prob( (a < X) & (X < b) )
+escribir("b) P(1/4 < X < 1/2) =", prob)
 
 escribir("")
 escribir("3.11")
@@ -61,20 +65,24 @@ escribir("b) P(0 < X <= 2) =", Fb-Fa)
 
 escribir("")
 escribir("3.17")
-f = Trozos((Fracción(1,2), (1<x) & (x<3)), 
-           (0, otroCaso))
-escribir("a) Área bajo curva =", cont.ProbTotal(f,x))
+fdp = Trozos((Fracción(1,2), (1<x) & (x<3)), 
+             (0, otroCaso))
+X = ContinuousRV(x, fdp)
+prob = cont.ProbTotal(fdp, x)
+escribir("a) Área bajo curva =", prob)
 a = 2
 b = Fracción('2.5')
-escribir("b) P(2 < X < 2.5) =", cont.Prob(f,x,a,b))
-b = Fracción('1.6')
-escribir("c) P(X <= 1.6) =", float(cont.ProbAcum(f,x,b)))
+prob = cont.Prob( (a < X) & (X < b) )
+escribir("b) P(2 < X < 2.5) =", prob)
+prob = redondear(cont.Prob(X <= 1.6), 1)
+escribir("c) P(X <= 1.6) =", prob)
 
 escribir("")
 escribir("3.19")
-f = Trozos((Fracción(1,2), (1<x) & (x<3)), 
-           (0, otroCaso))
-F = cont.ProbAcum(f,x)
+fdp = Trozos((Fracción(1,2), (1<x) & (x<3)), 
+             (0, otroCaso))
+X = ContinuousRV(x, fdp)
+F = cont.ProbAcum(X, x)
 escribir("F(x) =", F)
 Fb = F.subs(x, Fracción('2.5'))
 Fa = F.subs(x, 2)
@@ -82,12 +90,13 @@ escribir("P(2 < X < 2.5) =", Fb-Fa)
 
 escribir("")
 escribir("3.21")
-f = Trozos((x**Fracción(1,2), (0<x) & (x<1)), 
-           (0, otroCaso))
-k = 1/cont.ProbTotal(f,x)
+fdp = Trozos((x**Fracción(1,2), (0<x) & (x<1)), 
+             (0, otroCaso))
+k = 1/cont.ProbTotal(fdp,x)
 escribir("a) k =", k)
 escribir("b)")
-F = cont.ProbAcum(k*f,x)
+X = ContinuousRV(x, k*fdp)
+F = cont.ProbAcum(X,x)
 escribir("F(x) =", F)
 Fb = F.subs(x, 0.6)
 Fa = F.subs(x, 0.3)
@@ -108,48 +117,46 @@ escribir("b) P(-1 <= X < 3) =", Fb-Fa)
 
 escribir("")
 escribir("3.27")
-f = Trozos((Fracción(1,2000)*exp(-x/2000), (x>=0)), 
-           (0, otroCaso))
-F = cont.ProbAcum(f,x)
+fdp = Trozos((Fracción(1,2000)*exp(-x/2000), (x>=0)), 
+             (0, otroCaso))
+X = ContinuousRV(x, fdp)
+F = cont.ProbAcum(X,x)
 escribir("a) F(x) =", F)
-P = redondear(1 - F.subs(x,1000).evalf(), 4)
-escribir("b) P(X > 1000) =", P)
-P = redondear(F.subs(x,2000).evalf(), 4)
-escribir("c) P(X <= 2000) =", P)
+prob = redondear(1 - F.subs(x,1000).evalf(), 4)
+escribir("b) P(X > 1000) =", prob)
+prob = redondear(F.subs(x,2000).evalf(), 4)
+escribir("c) P(X <= 2000) =", prob)
 
 escribir("")
 escribir("3.29")
-f = Trozos((3*x**(-4), (x>1)), 
-           (0, otroCaso))
-escribir("a) Área bajo curva =", cont.ProbTotal(f,x))
-F = cont.ProbAcum(f,x)
+fdp = Trozos((3*x**(-4), (x>1)), 
+             (0, otroCaso))
+prob = cont.ProbTotal(fdp, x)
+escribir("a) Área bajo curva =", prob)
+X = ContinuousRV(x, fdp)
+F = cont.ProbAcum(X,x)
 escribir("b) F(x) =", F)
 P = redondear(1 - F.subs(x,4).evalf(), 4)
 escribir("c) P(X > 4) =", P)
 
 escribir("")
 escribir("3.31")
-f = Trozos((Fracción(1,4)*exp(-y/4), (y>=0)), 
-           (0, otroCaso))
-b = 6
-P = redondear(1 - cont.ProbAcum(f,y,b).evalf(), 4)
-escribir("a) P(Y > 6) =", P)
-a = 0
-b = 1
-P = redondear(cont.Prob(f,y,a,b).evalf(),4)
-escribir("b) P(0 < Y < 1) =", P)
+fdp = Trozos((Fracción(1,4)*exp(-y/4), (y>=0)), 
+             (0, otroCaso))
+Y = ContinuousRV(y, fdp)
+prob = redondear(cont.Prob(Y > 6).evalf(),4)
+escribir("a) P(Y > 6) =", prob)
+prob = redondear(cont.Prob((0 < Y) & (Y < 1)).evalf(),4)
+escribir("b) P(0 < Y < 1) =", prob)
 
 escribir("")
 escribir("3.33")
-f = Trozos(((y**4)*(1-y)**3, (0<=y) & (y<=1)), 
-           (0, otroCaso))
-k = 1/cont.ProbTotal(f,y)
+fdp = Trozos(((y**4)*(1-y)**3, (0<=y) & (y<=1)), 
+             (0, otroCaso))
+k = 1/cont.ProbTotal(fdp,y)
 escribir("a) k =", k)
-a = 0
-b = 0.5
-P = redondear(cont.Prob(k*f,y,a,b).evalf(),4)
-escribir("b) P(0 < Y < 0.5) =", P)
-a = 0.8
-b = 1
-P = redondear(cont.Prob(k*f,y,a,b).evalf(),4)
-escribir("c) P(0.8 < Y < 1) =", P)
+Y = ContinuousRV(y, k*fdp)
+prob = redondear(cont.Prob((0 < Y) & (Y < 0.5)).evalf(),4)
+escribir("b) P(0 < Y < 0.5) =", prob)
+prob = redondear(cont.Prob((0.8 < Y) & (Y < 1)).evalf(),4)
+escribir("c) P(0.8 < Y < 1) =", prob)
