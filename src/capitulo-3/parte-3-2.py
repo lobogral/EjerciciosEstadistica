a5f6c699dc191c6e8141d9946e78e0e15fbf9213
Camilo Sabogal <lobogral@gmail.com>
2021-08-21 14:23:01 -0500

    Desacoplo los fdp para mejorar sintaxis

--- src/capitulo-3/parte-3-2.py
+++ src/capitulo-3/parte-3-2.py
@@ -33,47 +33,50 @@ f = Trozos((24*x*y, (0<=x) & (x<=1) &
                     (0<=y) & (y<=1) &
                     (x + y <= 1)),             
             (0, otroCaso))
-
+conjCont.establecerFdp(f)
 intervalo = x + y < Fracción(1,2)
-prob = conjCont.Prob(f, intervalo)
+prob = conjCont.Prob(intervalo)
 escribir("a) P(X + Y < 1/2) =", prob)
-escribir("b) g(x) =", conjCont.ProbMarginal(f,x))
+escribir("b) g(x) =", conjCont.ProbMarginal(x))
 intervaloDep = y < Fracción(1,8)
 valIndep = Fracción(3,4)
-prob = conjCont.ProbCondicional(f, intervaloDep, x, valIndep)
+prob = conjCont.ProbCondicional(intervaloDep, x, valIndep)
 escribir("c) P( Y < 1/8 | X = 3/4 ) =", prob)
 
 escribir("")
 escribir("3.43")
 f = Trozos((4*x*y, (0<x) & (x<1) & (0<y) & (y<1)),        
            (0, otroCaso))
+conjCont.establecerFdp(f)
 intervalo = (0<=x) & (x<=Fracción(1,2)) 
 intervalo = intervalo & (Fracción(1,4)<=y) & (y<=Fracción(1,2))
-prob = conjCont.Prob(f, intervalo)
+prob = conjCont.Prob(intervalo)
 escribir("a) P(0 <= X <= 1/2, 1/4 < Y < 1/2) =", prob)
 intervalo = (x<y)
-prob = conjCont.Prob(f, intervalo)
+prob = conjCont.Prob(intervalo)
 escribir("b) P(X < Y) =", prob)
 
 escribir("")
 escribir("3.45")
 f = Trozos((1/y, (0<x) & (x<y) & (y<1)),        
            (0, otroCaso))
+conjCont.establecerFdp(f)
 intervalo = x + y > 1/2
-prob = conjCont.Prob(f, intervalo)
-escribir("P(X + Y > 1/2) =", redondear(prob, 4))
+prob = redondear(conjCont.Prob(intervalo), 4)
+escribir("P(X + Y > 1/2) =", prob)
 
 escribir("")
 escribir("3.47")
 f = Trozos((2, (0<x) & (x<y) & (y<1)),            
            (0, otroCaso))
+conjCont.establecerFdp(f)
 escribir("a)")
-escribir("g(x) =", conjCont.ProbMarginal(f,x))
-escribir("h(y) =", conjCont.ProbMarginal(f,y))
+escribir("g(x) =", conjCont.ProbMarginal(x))
+escribir("h(y) =", conjCont.ProbMarginal(y))
 escribir("g(x)h(y) != f(x,y), Dependientes")
 intervaloDep = (Fracción(1,4) < x) & (x < Fracción(1,2))
 valIndep = Fracción(3,4)
-prob = conjCont.ProbCondicional(f, intervaloDep, y, valIndep)
+prob = conjCont.ProbCondicional(intervaloDep, y, valIndep)
 escribir("b) P( 1/4 < X < 1/2 | Y = 3/4 ) =", prob)
 
 escribir("")
@@ -101,9 +104,10 @@ escribir("3.55")
 f = Trozos(((6-x-y)/8, (0<x) & (x<2) & 
                        (2<y) & (y<4)),            
            (0, otroCaso))
+conjCont.establecerFdp(f)
 intervaloDep = (1 < y) & (y < 3)
 valIndep = 1
-prob = conjCont.ProbCondicional(f, intervaloDep, x, valIndep)
+prob = conjCont.ProbCondicional(intervaloDep, x, valIndep)
 escribir("P( 1 < Y < 3 | X = 1 ) =", prob)
 
 escribir("")
@@ -114,6 +118,7 @@ f = Trozos((x*(y**2)*z, (0<x) & (x<1) &
            (0, otroCaso))
 k = 1/conjCont.ProbTotal(f)
 escribir("a) k =", k)
+conjCont.establecerFdp(k*f)
 intervalo = (x<Fracción(1,4)) & (y > Fracción(1,2)) & (1<z) & (z<2)
-prob = conjCont.Prob(k*f, intervalo)
+prob = conjCont.Prob(intervalo)
 escribir("b) P(X < 1/4, Y > 1/2, 1 < Z < 2) =", prob)
