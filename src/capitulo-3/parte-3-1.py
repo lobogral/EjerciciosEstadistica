7a6537d1fc6a52ad7c92894244832144d733e3d5
Camilo Sabogal <lobogral@gmail.com>
2021-08-27 12:04:36 -0500

    Agrego ejercicio en 3-1

--- src/capitulo-3/parte-3-1.py
+++ src/capitulo-3/parte-3-1.py
@@ -2,7 +2,7 @@ desde sympy importar Piecewise como Trozos
 desde sympy importar binomial como C
 desde sympy importar Eq
 desde sympy.abc importar x, y, w
-desde sympy.functions importar exp
+desde sympy.functions importar exp, factorial
 desde sympy.stats importar ContinuousRV
 desde fractions importar Fraction como Fracción
 desde redondeo importar redondear
@@ -43,7 +43,9 @@ escribir("b) P(1/4 < X < 1/2) =", prob)
 escribir("")
 escribir("3.11")
 f = (C(2,x)*C(5,3-x))/C(7,3)
-escribir("f(x) =", disc.Func2Troz(f, {x: [0,1,2]}))
+dom = {x: [0,1,2]}
+disc.establecerDpDom(f, dom)
+escribir("f(x) =", disc.Func2Troz())
 
 escribir("")
 escribir("3.13")
@@ -53,13 +55,15 @@ f = Trozos((0.41, Eq(x, 0)),
            (0.05, Eq(x, 3)), 
            (0.01, Eq(x, 4)))
 dom = {x: (0,4)}
-escribir("F(x)=", disc.ProbAcum(f, dom))
+disc.establecerDpDom(f, dom)
+escribir("F(x)=", disc.ProbAcum())
 
 escribir("")
 escribir("3.15")
 f = (C(2,x)*C(5,3-x))/C(7,3)
 dom = {x: [0,1,2]}
-F = disc.ProbAcum(f, dom)
+disc.establecerDpDom(f, dom)
+F = disc.ProbAcum()
 escribir("F(x)=", F)
 escribir("-- P(X = 1) = f(1)")
 escribir("-- F(1) = f(1) + f(0)")
@@ -120,7 +124,8 @@ f = Trozos((Fracción(1,27), Eq(w,-3)),
            (Fracción(12,27), Eq(w,1)),
            (Fracción(8,27), Eq(w,3)))
 dom = {w: [-3,-1,1,3]}
-F = disc.ProbAcum(f, dom)
+disc.establecerDpDom(f, dom)
+F = disc.ProbAcum()
 escribir("F(w)=", F)
 escribir("a) P(W > 0) =", 1-F.subs(w,0))
 escribir("-- P(-1 <= W < 3) = f(-1) + f(1)")
@@ -175,3 +180,14 @@ prob = redondear(cont.Prob((0 < Y) & (Y < 0.5)),4)
 escribir("b) P(0 < Y < 0.5) =", prob)
 prob = redondear(cont.Prob((0.8 < Y) & (Y < 1)),4)
 escribir("c) P(0.8 < Y < 1) =", prob)
+
+escribir("")
+escribir("3.35")
+f = exp(-6)*(6**x)/factorial(x)
+disc.establecerDpDom(f)
+subDom = {x: (0,8)}
+prob = redondear(1 - disc.Prob(subDom).evalf(), 4)
+escribir("a) P(X > 8) =", prob)
+subDom = {x: [2]}
+prob = redondear(disc.Prob(subDom).evalf(), 4)
+escribir("b) P(X = 2) =", prob)
