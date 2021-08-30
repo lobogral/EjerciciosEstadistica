387e84578bb7d4107db95c9b56d5b7c4ede40ea3
Camilo Sabogal <lobogral@gmail.com>
2021-08-29 20:43:15 -0500

    Ajusto ejercicios a comandos

--- src/capitulo-3/parte-3-2.py
+++ src/capitulo-3/parte-3-2.py
@@ -1,7 +1,8 @@
 desde sympy importar binomial como C
 desde sympy importar Piecewise como Trozos
 desde sympy importar Symbol como Simbolo
-desde sympy importar Eq
+desde sympy importar Eq como Ec
+desde sympy importar Contains como Pert
 desde fractions importar Fraction como Fracción
 desde decimal importar Decimal
 desde redondeo importar redondear
@@ -13,19 +14,21 @@ y = Simbolo("y", real=Verdadero)
 z = Simbolo("z", real=Verdadero)
 
 escribir("3.37")
-f = x*y
-c = conjDisc.ProbTotal(f, {x: (1,3), y: (1,3)})
+f = Trozos((x*y, Pert(x,{1,2,3}) & 
+                 Pert(y,{1,2,3})))
+c = conjDisc.ProbTotal(f)
 escribir("a) c =", 1/c)
-f = abs(x - y)
-c = conjDisc.ProbTotal(f, {x: [-2,0,2], y: [-2,3]})
+f = Trozos((abs(x - y), Pert(x,{-2,0,2}) & 
+                        Pert(y,{-2,3})))
+c = conjDisc.ProbTotal(f)
 escribir("b) c =", 1/c)
 
 escribir("")
 escribir("3.39")
-f = (C(3,x)*C(2,y)*C(3,4-x-y))/C(8,4)
-dom = {x: [0,1,2,3], y: [0,1,2]}
-conjDisc.establecerDpDom(f, dom)
-escribir("a) ", conjDisc.dp2Dist())
+f = Trozos(((C(3,x)*C(2,y)*C(3,4-x-y))/C(8,4), Pert(x,{0,1,2,3}) & 
+                                               Pert(y,{0,1,2})))
+conjDisc.establecerDp(f)
+escribir("a) ", conjDisc.dp2Dist(x,y))
 prob = conjDisc.Prob(x + y <= 2)
 escribir("b) P(X + Y <= 2) =", prob)
 
@@ -41,7 +44,7 @@ prob = conjCont.Prob(intervalo)
 escribir("a) P(X + Y < 1/2) =", prob)
 escribir("b) g(x) =", conjCont.ProbMarginal(x))
 intervaloDep = y < Fracción(1,8)
-eqDep = Eq(x, Fracción(3,4))
+eqDep = Ec(x, Fracción(3,4))
 prob = conjCont.ProbCondicional(intervaloDep, eqDep)
 escribir("c) P( Y < 1/8 | X = 3/4 ) =", prob)
 
@@ -77,7 +80,7 @@ escribir("g(x) =", conjCont.ProbMarginal(x))
 escribir("h(y) =", conjCont.ProbMarginal(y))
 escribir("g(x)h(y) != f(x,y), Dependientes")
 intervaloDep = (Fracción(1,4) < x) & (x < Fracción(1,2))
-eqDep = Eq(y, Fracción(3,4))
+eqDep = Ec(y, Fracción(3,4))
 prob = conjCont.ProbCondicional(intervaloDep, eqDep)
 escribir("b) P( 1/4 < X < 1/2 | Y = 3/4 ) =", prob)
 
@@ -86,21 +89,21 @@ escribir("3.49")
 dist = {(1, 1): 0.05, (2, 1): 0.05, (3, 1): 0.10, 
         (1, 2): 0.05, (2, 2): 0.10, (3, 2): 0.35, 
         (1, 3): 0.00, (2, 3): 0.20, (3, 3): 0.10}
-f, dom = conjDisc.dist2Dp(dist, [x, y])
-conjDisc.establecerDpDom(f, dom)
+f = conjDisc.dist2Dp(dist, [x, y])
+conjDisc.establecerDp(f)
 distMarginalX = conjDisc.ProbMarginal(x)
 escribir("a) g(x) =", distMarginalX)
 distMarginalY = conjDisc.ProbMarginal(y)
 escribir("b) h(y) =", distMarginalY)
-prob = redondear(conjDisc.ProbCondicional(Eq(y, 3), Eq(x, 2)), 4)
+prob = redondear(conjDisc.ProbCondicional(Ec(y, 3), Ec(x, 2)), 4)
 escribir("c) P(Y = 3 | X = 2) =", prob)
 
 escribir("")
 escribir("3.53")
-f = (C(4,x)*C(4,y)*C(4,3-x-y))/C(12,3)
-dom = {x: [0,1,2,3], y: [0,1,2,3]}
-conjDisc.establecerDpDom(f, dom)
-escribir("a)", conjDisc.dp2Dist())
+f = Trozos(( (C(4,x)*C(4,y)*C(4,3-x-y))/C(12,3), Pert(x, {0,1,2,3}) &
+                                                 Pert(y, {0,1,2,3})))
+conjDisc.establecerDp(f)
+escribir("a)", conjDisc.dp2Dist(x,y))
 prob = conjDisc.Prob(x + y >=2)
 escribir("b) P(X + Y <= 2) =", prob)
 
@@ -111,7 +114,7 @@ f = Trozos(((6-x-y)/8, (0<x) & (x<2) &
            (0, otroCaso))
 conjCont.establecerFdp(f)
 intervaloDep = (1 < y) & (y < 3)
-eqDep = Eq(x,1)
+eqDep = Ec(x,1)
 prob = conjCont.ProbCondicional(intervaloDep, eqDep)
 escribir("P( 1 < Y < 3 | X = 1 ) =", prob)
 
