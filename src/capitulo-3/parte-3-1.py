4014279d3378d21afbcee2cc6b8e98b2ec4645f0
Camilo Sabogal <lobogral@gmail.com>
2021-09-02 14:06:44 -0500

    Cambio ejercicios de acuerdo a comandos

--- src/capitulo-3/parte-3-1.py
+++ src/capitulo-3/parte-3-1.py
@@ -4,7 +4,6 @@ desde sympy importar Eq como Ec
 desde sympy importar Contains como Pert
 desde sympy.abc importar x, y, w
 desde sympy.functions importar exp, factorial
-desde sympy.stats importar ContinuousRV
 desde fractions importar Fraction como Fracción
 desde redondeo importar redondear
 desde distribuciones importar continua como cont
@@ -20,25 +19,25 @@ escribir("b) c =", c)
 
 escribir("")
 escribir("3.7")
-fdp = Trozos((x, (0<x) & (x<1)),
-             (2-x, (1<=x) & (x<2)), 
-             (0, otroCaso))
-X = ContinuousRV(x, fdp)
-prob = redondear(cont.Prob(X <= 1.2), 2)
+f = Trozos((x, (0<x) & (x<1)),
+           (2-x, (1<=x) & (x<2)), 
+           (0, otroCaso))
+cont.establecerFdp(f)
+prob = redondear(cont.Prob(x <= 1.2), 2)
 escribir("a) P(X < 1.2) =", prob)
-prob = redondear(cont.Prob( (0.5 < X) & (X < 1) ), 3)
+prob = redondear(cont.Prob( (0.5 < x) & (x < 1) ), 3)
 escribir("b) P(0.5 < X < 1) =", prob)
 
 escribir("")
 escribir("3.9")
-fdp = Trozos((Fracción(2,5)*(x+2), (0<x) & (x<1)), 
-             (0, otroCaso))
-X = ContinuousRV(x, fdp)
-prob = cont.ProbTotal(fdp)
+f = Trozos((Fracción(2,5)*(x+2), (0<x) & (x<1)), 
+           (0, otroCaso))
+cont.establecerFdp(f)
+prob = cont.ProbTotal(f)
 escribir("a) Área bajo curva =", prob)
 a = Fracción(1,4)
 b = Fracción(1,2)
-prob = cont.Prob( (a < X) & (X < b) )
+prob = cont.Prob( (a < x) & (x < b) )
 escribir("b) P(1/4 < X < 1/2) =", prob)
 
 escribir("")
@@ -75,24 +74,24 @@ escribir("b) P(0 < X <= 2) =", Fb-Fa)
 
 escribir("")
 escribir("3.17")
-fdp = Trozos((Fracción(1,2), (1<x) & (x<3)), 
-             (0, otroCaso))
-X = ContinuousRV(x, fdp)
-prob = cont.ProbTotal(fdp)
+f = Trozos((Fracción(1,2), (1<x) & (x<3)), 
+           (0, otroCaso))
+cont.establecerFdp(f)
+prob = cont.ProbTotal(f)
 escribir("a) Área bajo curva =", prob)
 a = 2
 b = Fracción('2.5')
-prob = cont.Prob( (a < X) & (X < b) )
+prob = cont.Prob( (a < x) & (x < b) )
 escribir("b) P(2 < X < 2.5) =", prob)
-prob = redondear(cont.Prob(X <= 1.6), 1)
+prob = redondear(cont.Prob(x <= 1.6), 1)
 escribir("c) P(X <= 1.6) =", prob)
 
 escribir("")
 escribir("3.19")
-fdp = Trozos((Fracción(1,2), (1<x) & (x<3)), 
-             (0, otroCaso))
-X = ContinuousRV(x, fdp)
-F = cont.ProbAcum(X)
+f = Trozos((Fracción(1,2), (1<x) & (x<3)), 
+           (0, otroCaso))
+cont.establecerFdp(f)
+F = cont.ProbAcum()
 escribir("F(x) =", F)
 Fb = F.subs(x, Fracción('2.5'))
 Fa = F.subs(x, 2)
@@ -100,13 +99,13 @@ escribir("P(2 < X < 2.5) =", Fb-Fa)
 
 escribir("")
 escribir("3.21")
-fdp = Trozos((x**Fracción(1,2), (0<x) & (x<1)), 
-             (0, otroCaso))
-k = 1/cont.ProbTotal(fdp)
+f = Trozos((x**Fracción(1,2), (0<x) & (x<1)), 
+           (0, otroCaso))
+k = 1/cont.ProbTotal(f)
 escribir("a) k =", k)
 escribir("b)")
-X = ContinuousRV(x, k*fdp)
-F = cont.ProbAcum(X)
+cont.establecerFdp(k*f)
+F = cont.ProbAcum()
 escribir("F(x) =", F)
 Fb = F.subs(x, 0.6)
 Fa = F.subs(x, 0.3)
@@ -132,10 +131,10 @@ escribir("b) P(-1 <= W < 3) =", Fb-Fa)
 
 escribir("")
 escribir("3.27")
-fdp = Trozos((Fracción(1,2000)*exp(-x/2000), (x>=0)), 
-             (0, otroCaso))
-X = ContinuousRV(x, fdp)
-F = cont.ProbAcum(X)
+f = Trozos((Fracción(1,2000)*exp(-x/2000), (x>=0)), 
+           (0, otroCaso))
+cont.establecerFdp(f)
+F = cont.ProbAcum()
 escribir("a) F(x) =", F)
 prob = redondear(1 - F.subs(x,1000).evalf(), 4)
 escribir("b) P(X > 1000) =", prob)
@@ -144,36 +143,36 @@ escribir("c) P(X <= 2000) =", prob)
 
 escribir("")
 escribir("3.29")
-fdp = Trozos((3*x**(-4), (x>1)), 
+f = Trozos((3*x**(-4), (x>1)), 
              (0, otroCaso))
-prob = cont.ProbTotal(fdp)
+prob = cont.ProbTotal(f)
 escribir("a) Área bajo curva =", prob)
-X = ContinuousRV(x, fdp)
-F = cont.ProbAcum(X)
+cont.establecerFdp(f)
+F = cont.ProbAcum()
 escribir("b) F(x) =", F)
 P = redondear(1 - F.subs(x,4).evalf(), 4)
 escribir("c) P(X > 4) =", P)
 
 escribir("")
 escribir("3.31")
-fdp = Trozos((0.25*exp(-y/4), (y>=0)), 
-             (0, otroCaso))
-Y = ContinuousRV(y, fdp)
-prob = redondear(cont.Prob(Y > 6).evalf(),4)
+f = Trozos((0.25*exp(-y/4), (y>=0)), 
+           (0, otroCaso))
+cont.establecerFdp(f)
+prob = redondear(cont.Prob(y > 6).evalf(),4)
 escribir("a) P(Y > 6) =", prob)
-prob = redondear(cont.Prob((0 < Y) & (Y < 1)).evalf(),4)
+prob = redondear(cont.Prob((0 < y) & (y < 1)).evalf(),4)
 escribir("b) P(0 < Y < 1) =", prob)
 
 escribir("")
 escribir("3.33")
-fdp = Trozos(((y**4)*(1-y)**3, (0<=y) & (y<=1)), 
-             (0, otroCaso))
-k = 1/cont.ProbTotal(fdp)
+f = Trozos(((y**4)*(1-y)**3, (0<=y) & (y<=1)), 
+           (0, otroCaso))
+k = 1/cont.ProbTotal(f)
 escribir("a) k =", k)
-Y = ContinuousRV(y, k*fdp)
-prob = redondear(cont.Prob((0 < Y) & (Y < 0.5)),4)
+cont.establecerFdp(k*f)
+prob = redondear(cont.Prob((0 < y) & (y < 0.5)),4)
 escribir("b) P(0 < Y < 0.5) =", prob)
-prob = redondear(cont.Prob((0.8 < Y) & (Y < 1)),4)
+prob = redondear(cont.Prob((0.8 < y) & (y < 1)),4)
 escribir("c) P(0.8 < Y < 1) =", prob)
 
 escribir("")
