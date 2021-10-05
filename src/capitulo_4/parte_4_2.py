8d323695bf7b6cb5004def060a67acb7450ce81b
Camilo Sabogal <lobogral@gmail.com>
2021-10-05 10:51:14 -0500

    Cambio nombres para mejorar lectura de codigo

--- src/capitulo_4/parte_4_2.py
+++ src/capitulo_4/parte_4_2.py
@@ -2,7 +2,7 @@ desde sympy importar Piecewise como Trozos
 desde sympy importar Symbol como Simbolo
 desde sympy.functions importar exp
 desde fractions importar Fraction como Frac
-desde estadistica.esperanza importar dist_E, cont_E
+desde estadistica.esperanza importar E_disc, E_cont
 desde redondeo.redondeo importar redondear
 
 t = Simbolo("t", real=Verdadero)
@@ -12,25 +12,25 @@ y = Simbolo("y", real=Verdadero)
 escribir("")
 escribir("4.33")
 dist = {4000:0.3, -1000:0.7}
-f = dist_E.dist_a_dp(dist, x)
-dist_E.establecer_dp(f)
-var = redondear(dist_E.Var(x), 0)
+f = E_disc.dist_a_dp(dist, x)
+E_disc.establecer_dp(f)
+var = redondear(E_disc.Var(x), 0)
 escribir("Var(X) = $", var)
 
 escribir("")
 escribir("4.35")
 dist = {2:0.01, 3:0.25, 4:0.4, 5:0.3, 6:0.04}
-f = dist_E.dist_a_dp(dist, x)
-dist_E.establecer_dp(f)
-var = redondear(dist_E.Var_Alter(x), 2)
+f = E_disc.dist_a_dp(dist, x)
+E_disc.establecer_dp(f)
+var = redondear(E_disc.Var_Alter(x), 2)
 escribir("Var(X) = ", var, "errores")
 
 escribir("")
 escribir("4.37")
 f = Trozos((2*(1-x), (0<x) & (x<1)),
            (0, otroCaso))
-cont_E.establecer_fdp(f)
-var = cont_E.Var(x)
+E_cont.establecer_fdp(f)
+var = E_cont.Var(x)
 escribir("Var(X) = $", var, "* 5000")
 
 escribir("")
@@ -38,8 +38,8 @@ escribir("4.39")
 f = Trozos((x, (0<x) & (x<1)),
            (2-x, (1<=x) & (x<2)), 
            (0, otroCaso))
-cont_E.establecer_fdp(f)
-var = cont_E.Var(x)
+E_cont.establecer_fdp(f)
+var = E_cont.Var(x)
 escribir("Var(X) = ", var, "* 100 horas")
 
 escribir("")
@@ -47,18 +47,18 @@ escribir("4.41")
 dist = {-3:Frac(1,6),
          6:Frac(1,2),
          9:Frac(1,3)}
-f = dist_E.dist_a_dp(dist, x)
-dist_E.establecer_dp(f)
-var = redondear(dist_E.desv((2*x+1)**2), 1)
+f = E_disc.dist_a_dp(dist, x)
+E_disc.establecer_dp(f)
+var = redondear(E_disc.desv((2*x+1)**2), 1)
 escribir("g(X) = (2X+1)^2, desv(g(X)) = ", var)
 
 escribir("")
 escribir("4.43")
 f = Trozos((Frac(1,4)*exp(-x/4), x>0),
            (0, otroCaso))
-cont_E.establecer_fdp(f)
-E = cont_E.E(3*x-2)
-var = cont_E.Var(3*x-2)
+E_cont.establecer_fdp(f)
+E = E_cont.E(3*x-2)
+var = E_cont.Var(3*x-2)
 escribir("Y = g(X) = 3X-2")
 escribir("E(Y) = ", E)
 escribir("Var(Y) = ", var)
@@ -68,9 +68,9 @@ escribir("4.45")
 dist = {(1, 1): 0.05, (2, 1): 0.05, (3, 1): 0.10, 
         (1, 2): 0.05, (2, 2): 0.10, (3, 2): 0.35, 
         (1, 3): 0.00, (2, 3): 0.20, (3, 3): 0.10}
-f = dist_E.dist_a_dp(dist, [x, y])
-dist_E.establecer_dp(f)
-cov = redondear(dist_E.Cov(),3)
+f = E_disc.dist_a_dp(dist, [x, y])
+E_disc.establecer_dp(f)
+cov = redondear(E_disc.Cov(),3)
 escribir("Cov(X,Y) =", cov)
 
 escribir("")
@@ -78,16 +78,16 @@ escribir("4.47")
 f = Trozos((Frac(2,3)*(x+2*y), (0<=x) & (x<=1) &
                                (0<=y) & (y<=1)),
            (0, otroCaso))
-cont_E.establecer_fdp(f)
-cov = redondear(cont_E.Cov().evalf(), 4)
+E_cont.establecer_fdp(f)
+cov = redondear(E_cont.Cov().evalf(), 4)
 escribir("Cov(X,Y) =", cov)
 
 escribir("")
 escribir("4.49")
 dist = {0:0.41, 1:0.37, 2:0.16, 3:0.05, 4:0.01}
-f = dist_E.dist_a_dp(dist, x)
-dist_E.establecer_dp(f)
-var = redondear(dist_E.Var(x), 4)
-desv = redondear(dist_E.desv(x), 4)
+f = E_disc.dist_a_dp(dist, x)
+E_disc.establecer_dp(f)
+var = redondear(E_disc.Var(x), 4)
+desv = redondear(E_disc.desv(x), 4)
 escribir("Var(X) =", var)
 escribir("desv(X) =", desv)
--- src/capitulo_4/parte_4_3.py
+++ src/capitulo_4/parte_4_3.py
@@ -2,7 +2,7 @@ desde sympy importar Piecewise como Trozos
 desde sympy importar Symbol como Simbolo
 desde sympy.functions importar exp
 desde fractions importar Fraction como Frac
-desde estadistica.esperanza importar dist_E, cont_E
+desde estadistica.esperanza importar E_disc, E_cont
 desde redondeo.redondeo importar redondear
 
 x = Simbolo("x", real=Verdadero)
@@ -10,10 +10,10 @@ y = Simbolo("y", real=Verdadero)
 
 escribir("4.51")
 dist = {2:0.01, 3:0.25, 4:0.4, 5:0.3, 6:0.04}
-f = dist_E.dist_a_dp(dist, x)
-dist_E.establecer_dp(f)
-EX = redondear(dist_E.E(x), 2)
-VarX = redondear(dist_E.Var(x), 2)
+f = E_disc.dist_a_dp(dist, x)
+E_disc.establecer_dp(f)
+EX = redondear(E_disc.E(x), 2)
+VarX = redondear(E_disc.Var(x), 2)
 escribir("Z = 3X-2")
 escribir("E(Z) = ", 3*EX - 2, "errores")
 escribir("Var(Z) = ", 3**2*VarX, "errores")
@@ -26,9 +26,9 @@ dist = {0:Frac(1,15),
         3:Frac(3,15),
         4:Frac(4,15),
         5:Frac(3,15)}
-f = dist_E.dist_a_dp(dist, x)
-dist_E.establecer_dp(f)
-EX = dist_E.E(x)
+f = E_disc.dist_a_dp(dist, x)
+E_disc.establecer_dp(f)
+EX = E_disc.E(x)
 escribir("Z = 1.65X-(1.20)*5+(0.75)(1.20)(5-X)")
 escribir("Z = 0.75X - 1.5")
 EZ = redondear(0.75*EX - 1.5, 2)
@@ -39,10 +39,10 @@ escribir("4.55")
 dist = {-3:Frac(1,6),
          6:Frac(1,2),
          9:Frac(1,3)}
-f = dist_E.dist_a_dp(dist, x)
-dist_E.establecer_dp(f)
-EX = dist_E.E(x)
-EX2 = dist_E.E(x**2)
+f = E_disc.dist_a_dp(dist, x)
+E_disc.establecer_dp(f)
+EX = E_disc.E(x)
+EX2 = E_disc.E(x**2)
 escribir("Z = (2X + 1)^2 = 4X^2 + 4X + 1")
 EZ = 4*EX2 + 4*EX + 1
 escribir("E(Z) = ", EZ)
@@ -52,10 +52,10 @@ escribir("4.59")
 dist = {(0, 0):Frac(3,28), (1, 0):Frac(9,28), (2, 0):Frac(3,28), 
         (0, 1):Frac(3,14), (1, 1):Frac(3,14), (2, 1):0, 
         (0, 2):Frac(1,28), (1, 2):0,          (2, 2):0}
-f = dist_E.dist_a_dp(dist, [x, y])
-dist_E.establecer_dp(f)
-E2XY2 = dist_E.E(2*x*y**2)
-EX2Y = dist_E.E(x**2*y)
+f = E_disc.dist_a_dp(dist, [x, y])
+E_disc.establecer_dp(f)
+E2XY2 = E_disc.E(2*x*y**2)
+EX2Y = E_disc.E(x**2*y)
 escribir("Z = XY^2 - X^2Y")
 escribir("E(Z) = ", E2XY2 - EX2Y)
 
@@ -72,9 +72,9 @@ dist = {1:Frac(1,6),
         4:Frac(1,6),
         5:Frac(1,6),
         6:Frac(1,6)}
-f = dist_E.dist_a_dp(dist, x)
-dist_E.establecer_dp(f)
-EX = dist_E.E(x)
+f = E_disc.dist_a_dp(dist, x)
+E_disc.establecer_dp(f)
+EX = E_disc.E(x)
 escribir("E(X + Y) =", EX + EX)
 escribir("E(X - Y) =", EX - EX)
 EXY = redondear((EX**2).evalf(), 2)
@@ -85,27 +85,27 @@ escribir("4.71")
 f = Trozos((Frac(2,7)*(x+2*y), (0<=x) & (x<=1) &
                                (1<=y) & (y<=2)),
            (0, otroCaso))
-cont_E.establecer_fdp(f)
-EXY3 = cont_E.E(x/(y**3))
-EX2Y = cont_E.E(x**2*y)
+E_cont.establecer_fdp(f)
+EXY3 = E_cont.E(x/(y**3))
+EX2Y = E_cont.E(x**2*y)
 escribir("E(X/(Y^3) + (X^2)Y) =", EXY3 + EX2Y)
 
 escribir("")
 escribir("4.73")
 f = Trozos((Frac(1,5), (0<=x) & (x<=5)),
            (0, otroCaso))
-cont_E.establecer_fdp(f)
-EX = redondear(cont_E.E(x).evalf(), 1)
-EX2 = redondear(cont_E.E(x**2).evalf(), 2)
+E_cont.establecer_fdp(f)
+EX = redondear(E_cont.E(x).evalf(), 1)
+EX2 = redondear(E_cont.E(x**2).evalf(), 2)
 escribir("E(X) =", EX)
 escribir("Var(X) =", EX2 - EX**2)
 
 escribir("")
 escribir("4.75")
 f = Trozos((Frac(9,16)*1/(4**(x+y)), (x>=0) & (y>=0)))
-dist_E.establecer_dp(f)
-EX = dist_E.E(x)
-VarX = dist_E.Var(x)
+E_disc.establecer_dp(f)
+EX = E_disc.E(x)
+VarX = E_disc.Var(x)
 escribir("a)") 
 escribir("E(X) =", EX, ", E(Y) =", EX) 
 escribir("Var(X) =", VarX, ", Var(Y) =", VarX)
@@ -117,9 +117,9 @@ escribir("")
 escribir("4.77")
 f = Trozos((Frac(1,4)*exp(-y/4), (y>=0)), 
            (0, otroCaso))
-cont_E.establecer_fdp(f)
-EY = cont_E.E(y)
-EY2 = cont_E.E(y**2)
+E_cont.establecer_fdp(f)
+EY = E_cont.E(y)
+EY2 = E_cont.E(y**2)
 escribir("a)")
 escribir("E(Y) =", EY)
 escribir("b)")
@@ -129,10 +129,10 @@ escribir("")
 escribir("4.79")
 f = Trozos((1, (7<=y) & (y<=8) ), 
            (0, otroCaso))
-cont_E.establecer_fdp(f)
-EY = cont_E.E(y)
-VarY = cont_E.Var(y)
-EeY = redondear(cont_E.E(exp(y)).evalf(), 2)
+E_cont.establecer_fdp(f)
+EY = E_cont.E(y)
+VarY = E_cont.Var(y)
+EeY = redondear(E_cont.E(exp(y)).evalf(), 2)
 escribir("De forma exacta, E(e^y) =", EeY)
 EeY = redondear((exp(EY)*(1 + VarY/2)).evalf(), 2)
 escribir("Por series de Taylor, E(e^y) =", EeY) 
