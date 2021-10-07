87234c3b6ced607f6750870b6db6feb271c0ba63
Camilo Sabogal <lobogral@gmail.com>
2021-10-07 09:01:54 -0500

    Uso snakecase en Esperanza

--- src/capitulo_4/parte_4_1.py
+++ src/capitulo_4/parte_4_1.py
@@ -20,24 +20,24 @@ dist = {20:Frac(1,5),
         30:Frac(1,5)}
 f = E_disc.dist_a_dp(dist, t)
 E_disc.establecer_dp(f)
-E = E_disc.E(t)
-escribir("E(T) = ", E, "centavos")
+E_T = E_disc.E(t)
+escribir("E(T) = ", E_T, "centavos")
 
 escribir("")
 escribir("4.5")
 dist = {0:0.41, 1:0.37, 2:0.16, 3:0.05, 4:0.01}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-E = redondear(E_disc.E(x), 2)
-escribir("E(X) = ", E, "imperfecciones")
+E_X = redondear(E_disc.E(x), 2)
+escribir("E(X) = ", E_X, "imperfecciones")
 
 escribir("")
 escribir("4.7")
 dist = {4000:0.3, -1000:0.7}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-E = redondear(E_disc.E(x), 0)
-escribir("E(X) = $", E)
+E_X = redondear(E_disc.E(x), 0)
+escribir("E(X) = $", E_X)
 
 escribir("")
 escribir("4.9")
@@ -46,8 +46,8 @@ dist = {3:Frac(8,52),
         0:Frac(36,52)}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-E = redondear(E_disc.E(x).evalf(), 2)
-escribir("E(X) = $", E)
+E_X = redondear(E_disc.E(x).evalf(), 2)
+escribir("E(X) = $", E_X)
 
 escribir("")
 escribir("4.11")
@@ -56,16 +56,16 @@ dist = {200000:0.002,
          50000:0.1}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-E = redondear(E_disc.E(x), 0) + 500
-escribir("E(X) = $", E)
+E_X = redondear(E_disc.E(x), 0) + 500
+escribir("E(X) = $", E_X)
 
 escribir("")
 escribir("4.13")
 f = Trozos((4/(pi*(1+x**2)), (0<x) & (x<1)),
            (0, otro_caso))
 E_cont.establecer_fdp(f)
-E = E_cont.E(x)
-escribir("E(X) =", E)
+E_X = E_cont.E(x)
+escribir("E(X) =", E_X)
 
 escribir("")
 escribir("4.15")
@@ -73,8 +73,8 @@ f = Trozos((x, (0<x) & (x<1)),
            (2-x, (1<=x) & (x<2)), 
            (0, otro_caso))
 E_cont.establecer_fdp(f)
-E = E_cont.E(x)*100
-escribir("E(X) =", E, "horas")
+E_X = E_cont.E(x)*100
+escribir("E(X) =", E_X, "horas")
 
 escribir("")
 escribir("4.17")
@@ -83,8 +83,8 @@ dist = {-3:Frac(1,6),
          9:Frac(1,3)}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-E = redondear(E_disc.E((2*x+1)**2).evalf(), 0)
-escribir("g(X) = (2X+1)^2, E(g(X)) =", E)
+E_g_X = redondear(E_disc.E((2*x+1)**2).evalf(), 0)
+escribir("g(X) = (2X+1)^2, E(g(X)) =", E_g_X)
 
 escribir("")
 escribir("4.19")
@@ -94,16 +94,16 @@ dist = {0:Frac(1,10),
         3:Frac(1,5)}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-E = redondear(E_disc.E(1200*x-50*x**2).evalf(), 0)
-escribir("g(X) = 1200X - 50X^2, E(g(X)) = $", E)
+E_g_X = redondear(E_disc.E(1200*x-50*x**2).evalf(), 0)
+escribir("g(X) = 1200X - 50X^2, E(g(X)) = $", E_g_X)
 
 escribir("")
 escribir("4.21")
 f = Trozos((2*(1-x), (0<x) & (x<1)),
            (0, otro_caso))
 E_cont.establecer_fdp(f)
-E = redondear(E_cont.E(x**2).evalf()*5000, 2)
-escribir("g(X) = X^2, E(g(X)) = $", E)
+E_g_X = redondear(E_cont.E(x**2).evalf()*5000, 2)
+escribir("g(X) = X^2, E(g(X)) = $", E_g_X)
 
 escribir("")
 escribir("4.23")
@@ -112,44 +112,44 @@ dist = {(2, 1): 0.10, (4, 1): 0.15,
         (2, 5): 0.10, (4, 5): 0.15}
 f = E_disc.dist_a_dp(dist, [x, y])
 E_disc.establecer_dp(f)
-E = redondear(E_disc.E(x*y**2).evalf(), 1)
-escribir("a) g(X,Y) = XY^2, E(g(X,Y)) =", E)
-E = redondear(E_disc.E(x).evalf(), 2)
-escribir("b) E(X) =", E)
-E = redondear(E_disc.E(y).evalf(), 2)
-escribir("c) E(Y) =", E)
+E_g_XY = redondear(E_disc.E(x*y**2).evalf(), 1)
+escribir("a) g(X,Y) = XY^2, E(g(X,Y)) =", E_g_XY)
+E_X = redondear(E_disc.E(x).evalf(), 2)
+escribir("b) E(X) =", E_X)
+E_Y = redondear(E_disc.E(y).evalf(), 2)
+escribir("c) E(Y) =", E_Y)
 
 escribir("")
 escribir("4.25")
 f = Trozos(( (nC(4,x)*nC(4,y)*nC(4,3-x-y))/nC(12,3), Con(0,1,2,3).pert(x) &
                                                      Con(0,1,2,3).pert(y)))
 E_disc.establecer_dp(f)
-E = E_disc.E(x+y)
-escribir("g(X,Y) = X+Y, E(g(X,Y)) =", E, "para jacks y reyes")
+E_g_XY = E_disc.E(x+y)
+escribir("g(X,Y) = X+Y, E(g(X,Y)) =", E_g_XY, "para jacks y reyes")
 
 escribir("")
 escribir("4.27")
 f = Trozos((Frac(1,2000)*exp(-x/2000), (x>=0)), 
            (0, otro_caso))
 E_cont.establecer_fdp(f)
-E = E_cont.E(x)
-escribir("E(X) =", E, "horas")
+E_X = E_cont.E(x)
+escribir("E(X) =", E_X, "horas")
 
 escribir("")
 escribir("4.29")
 f = Trozos((3*x**(-4), (x>1)), 
              (0, otro_caso))
 E_cont.establecer_fdp(f)
-E = E_cont.E(x)
-escribir("E(X) =", E)
+E_X = E_cont.E(x)
+escribir("E(X) =", E_X)
 
 escribir("")
 escribir("4.31")
 f = Trozos((5*(1-y)**4, (0<=y) & (y<=1)), 
              (0, otro_caso))
 E_cont.establecer_fdp(f)
-E = E_cont.E(y)
-escribir("a) E(Y) =", E)
+E_Y = E_cont.E(y)
+escribir("a) E(Y) =", E_Y)
 dist_cont.establecer_fdp(f)
-prob = simplify(dist_cont.prob(y>E))
+prob = simplify(dist_cont.prob(y>E_Y))
 escribir("a) P(Y > E) =", prob, "= (5/6)^5")
--- src/capitulo_4/parte_4_2.py
+++ src/capitulo_4/parte_4_2.py
@@ -14,24 +14,24 @@ escribir("4.33")
 dist = {4000:0.3, -1000:0.7}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-var = redondear(E_disc.Var(x), 0)
-escribir("Var(X) = $", var)
+Var_X = redondear(E_disc.Var(x), 0)
+escribir("Var(X) = $", Var_X)
 
 escribir("")
 escribir("4.35")
 dist = {2:0.01, 3:0.25, 4:0.4, 5:0.3, 6:0.04}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-var = redondear(E_disc.Var_Alter(x), 2)
-escribir("Var(X) = ", var, "errores")
+Var_X = redondear(E_disc.Var_Alter(x), 2)
+escribir("Var(X) = ", Var_X, "errores")
 
 escribir("")
 escribir("4.37")
 f = Trozos((2*(1-x), (0<x) & (x<1)),
            (0, otro_caso))
 E_cont.establecer_fdp(f)
-var = E_cont.Var(x)
-escribir("Var(X) = $", var, "* 5000")
+Var_X = E_cont.Var(x)
+escribir("Var(X) = $", Var_X, "* 5000")
 
 escribir("")
 escribir("4.39")
@@ -39,8 +39,8 @@ f = Trozos((x, (0<x) & (x<1)),
            (2-x, (1<=x) & (x<2)), 
            (0, otro_caso))
 E_cont.establecer_fdp(f)
-var = E_cont.Var(x)
-escribir("Var(X) = ", var, "* 100 horas")
+Var_X = E_cont.Var(x)
+escribir("Var(X) = ", Var_X, "* 100 horas")
 
 escribir("")
 escribir("4.41")
@@ -49,19 +49,19 @@ dist = {-3:Frac(1,6),
          9:Frac(1,3)}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-var = redondear(E_disc.desv((2*x+1)**2), 1)
-escribir("g(X) = (2X+1)^2, desv(g(X)) = ", var)
+Var_g_X = redondear(E_disc.desv((2*x+1)**2), 1)
+escribir("g(X) = (2X+1)^2, desv(g(X)) = ", Var_g_X)
 
 escribir("")
 escribir("4.43")
 f = Trozos((Frac(1,4)*exp(-x/4), x>0),
            (0, otro_caso))
 E_cont.establecer_fdp(f)
-E = E_cont.E(3*x-2)
-var = E_cont.Var(3*x-2)
+E_g_X = E_cont.E(3*x-2)
+Var_g_X = E_cont.Var(3*x-2)
 escribir("Y = g(X) = 3X-2")
-escribir("E(Y) = ", E)
-escribir("Var(Y) = ", var)
+escribir("E(Y) = ", E_g_X)
+escribir("Var(Y) = ", Var_g_X)
 
 escribir("")
 escribir("4.45")
@@ -70,8 +70,8 @@ dist = {(1, 1): 0.05, (2, 1): 0.05, (3, 1): 0.10,
         (1, 3): 0.00, (2, 3): 0.20, (3, 3): 0.10}
 f = E_disc.dist_a_dp(dist, [x, y])
 E_disc.establecer_dp(f)
-cov = redondear(E_disc.Cov(),3)
-escribir("Cov(X,Y) =", cov)
+Cov = redondear(E_disc.Cov(),3)
+escribir("Cov(X,Y) =", Cov)
 
 escribir("")
 escribir("4.47")
@@ -79,15 +79,15 @@ f = Trozos((Frac(2,3)*(x+2*y), (0<=x) & (x<=1) &
                                (0<=y) & (y<=1)),
            (0, otro_caso))
 E_cont.establecer_fdp(f)
-cov = redondear(E_cont.Cov().evalf(), 4)
-escribir("Cov(X,Y) =", cov)
+Cov = redondear(E_cont.Cov().evalf(), 4)
+escribir("Cov(X,Y) =", Cov)
 
 escribir("")
 escribir("4.49")
 dist = {0:0.41, 1:0.37, 2:0.16, 3:0.05, 4:0.01}
 f = E_disc.dist_a_dp(dist, x)
 E_disc.establecer_dp(f)
-var = redondear(E_disc.Var(x), 4)
-desv = redondear(E_disc.desv(x), 4)
-escribir("Var(X) =", var)
-escribir("desv(X) =", desv)
+Var_X = redondear(E_disc.Var(x), 4)
+desv_X = redondear(E_disc.desv(x), 4)
+escribir("Var(X) =", Var_X)
+escribir("desv(X) =", desv_X)
