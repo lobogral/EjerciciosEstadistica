c8f13eb69c3683039cf37af1f104f9f0d150a99a
Camilo Sabogal <lobogral@gmail.com>
2021-11-11 22:55:24 -0500

    Agrego ejercicios de bin neg y geometrica en cap 5

--- src/capitulo_5/parte_5_3.py
+++ src/capitulo_5/parte_5_3.py
@@ -1,11 +1,24 @@
+from estadistica.distribuciones.discretas.bin_neg import BinNeg
 from estadistica.distribuciones.discretas.poisson import Poisson
+from estadistica.distribuciones.discretas.geometrica import Geometrica
 from redondeo.redondeo import redondear
 from sympy import Symbol
 from sympy import Eq
 
 x = Symbol("x", real=True)
 
+print("5.51")
+bin_neg = BinNeg(x, 0.3, 5)
+print("P(X = 10) =", redondear(bin_neg.prob(Eq(x, 10)).evalf(), 4))
+
+print("")
 print("5.53")
 poisson = Poisson(x, 5)
 print("a) P(X > 5) =", redondear(poisson.prob(x > 5).evalf(), 4))
 print("b) P(X = 0) =", redondear(poisson.prob(Eq(x, 0)).evalf(), 4))
+
+print("")
+print("5.57")
+geometrica = Geometrica(x, 0.7)
+print("a) P(X = 3) =", redondear(geometrica.prob(Eq(x, 3)).evalf(), 4))
+print("b) P(X < 4) =", redondear(geometrica.prob(x < 4).evalf(), 4))
